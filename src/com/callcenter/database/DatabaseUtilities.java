f66c27c9267ae41b64ea45a0e4f29b17922c33bb
Burak ATLAY <76514047+burakatlay@users.noreply.github.com>
2023-06-04 15:26:49 +0300

    Update DatabaseUtilities.java

--- src/com/callcenter/database/DatabaseUtilities.java
+++ src/com/callcenter/database/DatabaseUtilities.java
@@ -16,16 +16,16 @@ import java.sql.SQLException;
 public class DatabaseUtilities {
 
 //    public static String url = "jdbc:postgresql://localhost:5432/callcenter";
-//    public static String name = "postgres";
-//    public static String password = "52579485";
+//    public static String name = "";
+//    public static String password = "";
     
-//    public static String url = "jdbc:mysql://213.238.183.151:3306/burakatl_callcenter";
-//    public static String name = "burakatl_root";
-//    public static String password = "By52579485";
+//    public static String url = "";
+//    public static String name = "";
+//    public static String password = "";
     
     public static String url = "jdbc:mysql://localhost:3306/call_center";
-    public static String name = "root";
-    public static String password = "52579485";
+    public static String name = "";
+    public static String password = "";
     
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
